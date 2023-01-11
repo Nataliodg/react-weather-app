fa731be4fd25ae56a9e56297a0d4718ba7d3cd35
nataliodg <natadg@outlook.com>
2023-01-11 11:11:18 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react'
+import Icons from './components/Icons';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState('')
+  const [location, setLocation] = useState('roma')
+  const [icon, setIcon] = useState('')
+
+
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&lang=es&units=metric&appid=${import.meta.env.VITE_REACT_APP_API_KEY}`
+
+  const getData = async () => {
+    await fetch(URL)
+      .then(response => { return response.json() })
+      .then(data => {
+        console.log(data)
+
+        if (data.cod >= 400) {
+          setData(false)
+        } else {
+          setIcon(data.weather[0].icon)
+          setData(data)
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  const handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      setLocation(e.target.value)
+    }
+  }
+  useEffect(() => {
+    getData()
+  }, [location]) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="search">
+        <input
+          onKeyDown={handleSearch}
+          placeholder='Enter Location'
+          type="text" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="container">
+        {(data) ? (
+          <div className="top">
+            <div className="location">
+              <p>{data.name}</p>
+            </div>
+            <div className="temp">
+              {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+            </div>
+            <div className="description">
+              {data.weather ? <p>{data.weather[0].main}</p> : null}
+            </div>
+            <div className="img">
+              {data.weather ? <img className='icon' src={Icons(icon)} alt="icon-weather" /> : null}
+            </div>
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
+                <p>Feels Like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          </div>) : (
+          <h1>City not found</h1>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Icons.jsx
@@ -0,0 +1,82 @@
+const Icons = (icon) => {
+switch (icon) {
+    case '11d':
+        icon='Icons/thunderstorms-rain.svg'
+        console.log("TORMENTA");
+        break; 
+    case '11n':
+            icon='Icons/thunderstorms-rain.svg'
+            console.log("TORMENTA");
+            break;    
+    case '09d':
+        icon='Icons/partly-cloudy-day-drizzle.svg'
+        console.log('LLOVIZNA');
+        break;
+    case '09n':
+            icon='Icons/partly-cloudy-night-rain.svg'
+            console.log('LLOVIZNA');
+            break;
+    case '10d':
+        icon='Icons/rain.svg'
+        console.log('LLUVIA');
+        break;
+    case '10n':
+            icon='Icons/partly-cloudy-night-rain.svg'
+            console.log('LLUVIA');
+            break;
+    case '13d':
+        icon='Icons/snow.svg'
+        console.log('NIEVE');
+        break;
+    case '13n':
+            icon='Icons/snow.svg'
+            console.log('NIEVE');
+            break;                          
+    case '01d':
+        icon='Icons/clear-day.svg'
+        console.log('DESPEJADO');
+        break;
+    case '01n':
+        icon='Icons/clear-night.svg'
+        console.log('DESPEJADO');
+        break; 
+    case '02d':
+        icon='Icons/partly-cloudy-day-fog.svg'
+        console.log('NUBES');
+        break;
+    case "02n":
+        icon='Icons/partly-cloudy-night-fog.svg'
+        console.log('NUBES');
+        break; 
+    case '03d':
+        icon='Icons/cloudy.svg'
+        console.log('NUBES');
+        break;
+    case '03n':
+        icon='Icons/cloudy.svg'
+        console.log('NUBES');
+        break;
+    case '04d':
+        icon='Icons/fog.svg'
+        console.log('NUBES');
+        break;  
+    case '04n':
+        icon='Icons/fog.svg'
+        console.log('NUBES');
+        break;    
+    case '50d':
+        icon='Icons/mist.svg'
+        console.log('HUMO');
+        break;
+    case '50n':
+        icon='Icons/mist.svg'
+        console.log('HUMO');
+        break;      
+    default:
+        icon=null
+        console.log('LIMPIO');    
+}
+return icon
+}
+
+export default Icons
